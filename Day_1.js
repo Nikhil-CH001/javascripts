8fdcd2452caeaa55f1f86df073ba739e1272712b
Nikhil <nikhil@Nikhil>
2025-03-10 13:13:46 +0545

    first commit

---
+++ Day_1.js
@@ -0,0 +1,74 @@
+// console.log("hello scripter\n")
+// console.log(1+2)
+
+// const beauty = "good girl"
+// let body = "workout"
+
+// console.log("sumit is "+beauty)
+
+
+
+//varaible
+
+let bottle = "coke"
+let heat=100
+console.log(`i have ${bottle} in my bottle with ${heat} boiling point`)
+
+
+//array
+
+// let friend = ["nikhil","sudip","sumit","nishanta","kamal"]
+// let brand =["nike","addidas","woo"]
+// brand[0]="akkash"
+// console.log(brand)
+// brand.unshift("calibeer")
+// console.log(brand)
+// brand.shift()
+// console.log(brand)
+
+let country = ["Nepal","India","Pakistan"]
+country[0]="America"
+country[1]="ukrain"
+country[2]="Russia"
+
+console.log(country)
+console.log(country.length)
+
+
+//slice -> to remove the content with the help of indexing
+// let nums = [1,2,3,4,5,6]
+// console.log(nums.length)
+// console.log(nums.slice(0,1))
+
+
+// let number = [2,4,6,8,10,12]
+
+// console.log(number.slice(3,6))
+// console.log(number)
+
+
+// splice -> cut garx and content add garna milx and org array lay modify garx
+// let Numbers= [1,2,3,4,5]
+// Numbers.splice(0,5,"a","e","i","o","u")
+// console.log(Numbers)
+
+// console.log(friend[0])
+
+let msg = "I am a good boy"
+let new_msg = msg.split(" ");
+console.log(new_msg)
+
+
+
+
+
+
+//object
+
+let MyProfile = {
+    name: "Nikhil Chaudhary",
+    age: 20,
+    address: "Barahakshetra-5, Kalabanjar"
+}
+
+console.log("Name: "+MyProfile.name)
